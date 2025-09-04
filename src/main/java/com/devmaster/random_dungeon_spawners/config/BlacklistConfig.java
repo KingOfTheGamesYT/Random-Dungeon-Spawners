c01e47bb6dcdacf93e95926184d1c0ee248893e5
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2025-09-04 14:46:55 -0400

    ported the config
    works the same as in 1.16.5, but is structured differently code wise

--- src/main/java/com/devmaster/random_dungeon_spawners/config/BlacklistConfig.java
+++ src/main/java/com/devmaster/random_dungeon_spawners/config/BlacklistConfig.java
@@ -1,6 +1,6 @@
 package com.devmaster.random_dungeon_spawners.config;
 
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.resources.ResourceLocation;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BlacklistConfig {
-
     public static ForgeConfigSpec COMMON_CONFIG;
     public static ForgeConfigSpec.ConfigValue<List<? extends String>> ENTITY_BLACKLIST;
 
@@ -20,16 +19,19 @@ public class BlacklistConfig {
 
         ENTITY_BLACKLIST = builder.defineList(
                 "blacklistedEntities",
-                Arrays.asList("minecraft:ender_dragon", "minecraft:wither", "minecraft:elder_guardian"),
-                o -> o instanceof String && isValidEntity((String) o)
+                Arrays.asList(
+                        "minecraft:wither",
+                        "minecraft:ender_dragon",
+                        "minecraft:elder_guardian"
+                ),
+                obj -> {
+                    if (!(obj instanceof String)) return false;
+                    ResourceLocation id = ResourceLocation.tryParse((String) obj);
+                    return id != null && ForgeRegistries.ENTITY_TYPES.containsKey(id);
+                }
         );
 
         builder.pop();
         COMMON_CONFIG = builder.build();
-    }
-
-    private static boolean isValidEntity(String id) {
-        ResourceLocation rl = ResourceLocation.tryCreate(id);
-        return rl != null && ForgeRegistries.ENTITIES.containsKey(rl);
     }
 }
